3bab4884ffc2a7981bb8c44d28bdd174e9b3e03c
Trisha Gupta <tgupta@tgupta-4whv2t.local>
2020-01-21 21:11:21 +0530

    Added email extraction and text scan feature

---
+++ emailExtracter2.py
@@ -0,0 +1,44 @@
+import datetime
+import email
+import imaplib
+import mailbox
+
+
+EMAIL_ACCOUNT = "fyphkutest"
+PASSWORD = "helloworld123"
+
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login(EMAIL_ACCOUNT, PASSWORD)
+mail.list()
+mail.select('inbox')
+result, data = mail.uid('search', None, "UNSEEN") # (ALL/UNSEEN)
+i = len(data[0].split())
+
+for x in range(i):
+    latest_email_uid = data[0].split()[x]
+    result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+    # result, email_data = conn.store(num,'-FLAGS','\\Seen')
+    # this might work to set flag to seen, if it doesn't already
+    raw_email = email_data[0][1]
+    raw_email_string = raw_email.decode('utf-8')
+    email_message = email.message_from_string(raw_email_string)
+
+    # Header Details
+    date_tuple = email.utils.parsedate_tz(email_message['Date'])
+    if date_tuple:
+        local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+        local_message_date = "%s" %(str(local_date.strftime("%a, %d %b %Y %H:%M:%S")))
+    email_from = str(email.header.make_header(email.header.decode_header(email_message['From'])))
+    email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
+    subject = str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
+
+    # Body details
+    for part in email_message.walk():
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True)
+            file_name = "email.txt"
+            output_file = open(file_name, 'w')
+            output_file.write("Body: \n\n%s" %(body.decode('utf-8')))
+            output_file.close()
+        else:
+            continue
--- index.py
+++ index.py
@@ -3,18 +3,41 @@
 #importing libraries
 from sklearn.externals import joblib
 import inputScript
-
+import imaplib, email
+import os, pkg_resources
+import importlib.resources as pkg_resources
 #load the pickle file
 classifier = joblib.load('final_models/rf_final.pkl')
 
 #input url
-print("enter url")
-url = input()
+print("Checking...")
+#url = input()
+def extractURL(fileName):
 
+    wordsInLine = []
+    tempWord = []
+    urlList = []
+
+    #open up the file containing the email
+    file = open(fileName)
+    for line in file:
+        #create a list that contains each word in each line
+        wordsInLine = line.split(' ')
+        #For each word try to split it with :
+        for word in wordsInLine:
+            tempWord = word.split(":")
+            #Check to see if the word is a URL
+            if len(tempWord) == 2:
+                if tempWord[0] == "http" or tempWord[0] == "https":
+                    urlList.append(word)
+
+    file.close()
+
+    return urlList[0]
 #checking and predicting
-checkprediction = inputScript.main(url)
+checkprediction = inputScript.main(extractURL('email.txt'))
 prediction = classifier.predict(checkprediction)
 if (prediction == 1):
-    print("URL may be a phishing attack!")
+    print("Email contains URL that may be a phishing attack!")
 else:
-    print("URL seems safe")
+    print("Email contains URL that seems safe")
